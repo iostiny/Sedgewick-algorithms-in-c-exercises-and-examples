e9f8235df528efd492488c248c11de87747e7a01
Rick Shi <iostiny@yahoo.com>
2017-06-29 15:18:10 +0800

    Should be 'm' but not 'n' here

--- 03-elementary_data_structures/3.07-compound_data_structures/exercises/ex_3.63.c
+++ 03-elementary_data_structures/3.07-compound_data_structures/exercises/ex_3.63.c
@@ -5,21 +5,21 @@ int ***malloc3d(int m, int n, int p)
 {
   int i, j;
   int ***t = malloc(m * sizeof(int **));
-  
-  for (i = 0; i < n; i++)
-    {
-      t[i] = malloc(n * sizeof(int *));
-      for (j = 0; j < n; j++)
-	t[i][j] = malloc(p * sizeof(int));
-    }
+
+  for (i = 0; i < m; i++)
+  {
+    t[i] = malloc(n * sizeof(int *));
+    for (j = 0; j < n; j++)
+      t[i][j] = malloc(p * sizeof(int));
+  }
   return t;
 }
 
-int main(void) 
+int main(void)
 {
   int m, n, o;
   int ***a;
-  
+
   a = malloc3d(m, n, o);
   return 0;
 }
